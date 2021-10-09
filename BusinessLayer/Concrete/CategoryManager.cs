63dd842f340186bec500bbdb85f8a95156f3cfc3
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-10-10 00:18:15 +0300

    EntityFramework Refactoring

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        EfCategoryRepository _categoryRepository;
+        //to do DependencyInjection
+        public CategoryManager()
+        {
+            _categoryRepository = new EfCategoryRepository();
+        }
+        public void Add(Category category)
+        {
+            _categoryRepository.Insert(category);
+        }
+
+        public void Delete(Category category)
+        {
+            _categoryRepository.Delete(category);
+        }
+
+        public Category GetById(int id)
+        {
+            return _categoryRepository.GetById(id);
+        }
+
+        public List<Category> GetList()
+        {
+            return _categoryRepository.GetListAll();
+        }
+
+        public void Update(Category category)
+        {
+            _categoryRepository.Update(category);
+        }
+    }
+}
