6eb206dbc5916d3aa2bdc4661b88daf765dc5d51
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-10-10 18:44:07 +0300

    Bloglar Listelendi.

---
+++ BusinessLayer/Concrete/BlogManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+    public class BlogManager : IBlogService
+    {
+        private IBlogDal _blogDal;
+
+        public BlogManager(IBlogDal blogDal)
+        {
+            _blogDal = blogDal;
+        }
+
+        public void Add(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Blog> GetList()
+        {
+            return _blogDal.GetListAll();
+        }
+
+        public Blog GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- CoreProject/Controllers/BlogController.cs
+++ CoreProject/Controllers/BlogController.cs
@@ -3,14 +3,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
 
 namespace CoreProject.Controllers
 {
     public class BlogController : Controller
     {
+        private BlogManager _blogManager = new BlogManager(new EfBlogRepository());
         public IActionResult Index()
         {
-            return View();
+            var values = _blogManager.GetList();
+            return View(values);
         }
     }
 }
