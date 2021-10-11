3eb1db0c021e783f2825b08d1bdafad875cd031d
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-10-12 00:26:31 +0300

    Bloglara Ait Yorumlar Listelendi.

---
+++ CoreProject/ViewComponents/Comment/CommentListByBlog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject.ViewComponents.Comment
+{
+    public class CommentListByBlog:ViewComponent
+    {
+        private CommentManager _commentManager = new CommentManager(new EfCommentRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = _commentManager.GetList(3);
+            return View(values);
+        }
+    }
+}
