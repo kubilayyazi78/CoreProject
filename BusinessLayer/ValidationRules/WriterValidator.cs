b2704708458682a292425eac4ad31f949c71b545
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-10-12 18:13:59 +0300

    Kayıt işlemler, sweetalert kullanımı ve fluentvalidation uygulandı.

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer.Concrete;
+using FluentValidation;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Yazar Adı Soyadı Boş Geçilemez");
+            RuleFor(x => x.Mail).NotEmpty().WithMessage("Mail Boş Geçilemez");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Şifre Boş Geçilemez");
+            RuleFor(x => x.Name).MinimumLength(2).WithMessage("2 karakterden fazla olmalı");
+            RuleFor(x => x.Name).MaximumLength(50).WithMessage("En fazla 50 karakter olabilir");
+        }
+    }
+}
---
+++ CoreProject/Controllers/RegisterController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+
+namespace CoreProject.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private WriterManager writerManager = new WriterManager(new EfWriterRepository());
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Writer writer)
+        {
+            WriterValidator writerValidator = new WriterValidator();
+            ValidationResult results = writerValidator.Validate(writer);
+            if (results.IsValid)
+            {
+                writer.Status = true;
+                writer.About = "Deneme Test";
+                writerManager.Add(writer);
+                return RedirectToAction("Index", "Blog");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName,item.ErrorMessage);
+                }
+            }
+
+            return View();
+        }
+    }
+}
