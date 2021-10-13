73f7163d60ef65311f1851b01dc8972f763ba2fd
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-10-13 18:19:01 +0300

    Hakkımızda sayfası yapıldı.

---
+++ CoreProject/Controllers/AboutController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+
+namespace CoreProject.Controllers
+{
+    public class AboutController : Controller
+    {
+        private AboutManager _aboutManager = new AboutManager(new EfAboutRepository());
+        public IActionResult Index()
+        {
+            var values = _aboutManager.GetList();
+            return View(values);
+        }
+
+        public PartialViewResult SocialMediaAbout()
+        {
+
+            return PartialView();
+        }
+    }
+}
