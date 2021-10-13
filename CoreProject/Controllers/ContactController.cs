a6629aed45f84812f7ba362a49c2dc9c8e5d32b4
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-10-13 18:45:01 +0300

    İletişim sayfası tasarlandı.

---
+++ CoreProject/Controllers/ContactController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+
+namespace CoreProject.Controllers
+{
+    public class ContactController : Controller
+    {
+        private ContactManager _contactManager = new ContactManager(new EfContactRepository());
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Contact contact)
+        {
+            contact.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
+            contact.Status = true;
+            _contactManager.Add(contact);
+            return RedirectToAction("Index", "Blog");
+        }
+
+    }
+}
