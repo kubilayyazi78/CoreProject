3554139d8a67f852f5935c0747a323188770e912
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-10-30 14:46:13 +0300

    Dashboard işlemleri yapıldı.

---
+++ CoreProject/ViewComponents/Blog/BlogListDashboard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject.ViewComponents.Blog
+{
+    public class BlogListDashboard : ViewComponent
+    {
+        private BlogManager _blogManager = new BlogManager(new EfBlogRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _blogManager.GetListWithCategory();
+            return View(values);
+        }
+    }
+}
---
+++ CoreProject/ViewComponents/Category/CategoryListDashboard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject.ViewComponents.Category
+{
+    public class CategoryListDashboard:ViewComponent
+    {
+        private CategoryManager _categoryManager = new CategoryManager(new EfCategoryRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _categoryManager.GetList();
+            return View(values);
+        }
+    }
+}
