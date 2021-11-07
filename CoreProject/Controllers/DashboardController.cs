0a25decce04693fdabe8806e79606582dd5909bc
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-11-07 14:22:46 +0300

    Dashboard istatistikler yansıtıldı.

--- CoreProject/Controllers/DashboardController.cs
+++ CoreProject/Controllers/DashboardController.cs
@@ -3,15 +3,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataAccessLayer.Concrete;
 using Microsoft.AspNetCore.Authorization;
 
 namespace CoreProject.Controllers
 {
     public class DashboardController : Controller
     {
+        private Context _context = new Context();
         [AllowAnonymous]
         public IActionResult Index()
         {
+            ViewBag.Count = _context.Blogs.Count().ToString();
+            ViewBag.WriterCount = _context.Blogs.Count(x => x.WriterId == 1).ToString();
+            ViewBag.CategoryCount = _context.Categories.Count().ToString();
             return View();
         }
     }
