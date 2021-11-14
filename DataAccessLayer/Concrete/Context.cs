4f9634025008436b06f3570d0029bbff683a2478
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-11-14 19:00:05 +0300

    Message entity tanımlandı.

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -24,5 +24,9 @@ namespace DataAccessLayer.Concrete
         public DbSet<BlogRayting> BlogRaytings { get; set; }
 
         public DbSet<Notification> Notifications { get; set; }
+        public DbSet<Message> Messages { get; set; }
+
+
+
     }
 }
