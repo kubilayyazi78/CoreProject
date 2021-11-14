96aafbdf3e8a2d025e841e2748268885ddbf2a79
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-11-14 17:51:32 +0300

    Notification entity oluşturuldu.

---
+++ BusinessLayer/Concrete/NotificationManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+    public class NotificationManager : INotificationService
+    {
+        private INotificationDal _notificationDal;
+
+        public NotificationManager(INotificationDal notificationDal)
+        {
+            _notificationDal = notificationDal;
+        }
+        public void Add(Notification entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Notification entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Notification entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Notification> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Notification GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
