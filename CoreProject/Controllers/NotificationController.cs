e31fb4886f7585354297622db6dc1b3b5a884b42
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-11-20 23:53:44 +0300

    Bildirimler listelendi.

--- CoreProject/Controllers/NotificationController.cs
+++ CoreProject/Controllers/NotificationController.cs
@@ -3,14 +3,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
 
 namespace CoreProject.Controllers
 {
+    [AllowAnonymous]
     public class NotificationController : Controller
     {
+        private NotificationManager _notificationManager = new NotificationManager(new EfNotificationRepository());
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult AllNotification()
+        {
+            var values = _notificationManager.GetList();
+
+            return View(values);
         }
     }
 }
