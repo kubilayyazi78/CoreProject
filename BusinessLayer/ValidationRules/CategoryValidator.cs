2a1a79a1082aa0d38a7aebf02867b6eb8441b27e
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-12-04 23:30:00 +0300

    Kategori Ekleme İşlemi Gerçekleştirildi.

---
+++ BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer.Concrete;
+using FluentValidation;
+
+namespace BusinessLayer.ValidationRules
+{
+   public class CategoryValidator:AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Kategori adı boş bırakılamaz...");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Açıklama boş bırakılamaz...");
+            RuleFor(x => x.Name).MaximumLength(50).WithMessage("Kategori adı 50 karakterden fazla olmamalıdır...");
+            RuleFor(x => x.Name).MinimumLength(2).WithMessage("Kategori adı en az 2 karakter olmalıdır...");
+        }
+    }
+}
--- CoreProject/Areas/Admin/Controllers/CategoryController.cs
+++ CoreProject/Areas/Admin/Controllers/CategoryController.cs
@@ -4,7 +4,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using X.PagedList;
 
 namespace CoreProject.Areas.Admin.Controllers
@@ -13,10 +16,35 @@ namespace CoreProject.Areas.Admin.Controllers
     public class CategoryController : Controller
     {
         private CategoryManager _categoryManager = new CategoryManager(new EfCategoryRepository());
-        public IActionResult Index(int page =1)
+        public IActionResult Index(int page = 1)
         {
-            var values = _categoryManager.GetList().ToPagedList(page,3);
+            var values = _categoryManager.GetList().ToPagedList(page, 3);
             return View(values);
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Category category)
+        {
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult validationResult = categoryValidator.Validate(category);
+            if (validationResult.IsValid)
+            {
+                category.Status = true;
+                _categoryManager.Add(category);
+                return RedirectToAction("Index", "Category");
+            }
+            else
+            {
+                foreach (var item in validationResult.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
         }
     }
 }
