f835df09184cde6aec77e024f4c94cf84546cdfe
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-12-04 18:51:15 +0300

    Sayfalama işlemi gerçekleştirildi.

--- CoreProject/Areas/Admin/Controllers/CategoryController.cs
+++ CoreProject/Areas/Admin/Controllers/CategoryController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using X.PagedList;
 
 namespace CoreProject.Areas.Admin.Controllers
 {
@@ -12,9 +13,9 @@ namespace CoreProject.Areas.Admin.Controllers
     public class CategoryController : Controller
     {
         private CategoryManager _categoryManager = new CategoryManager(new EfCategoryRepository());
-        public IActionResult Index()
+        public IActionResult Index(int page =1)
         {
-            var values = _categoryManager.GetList();
+            var values = _categoryManager.GetList().ToPagedList(page,3);
             return View(values);
         }
     }
