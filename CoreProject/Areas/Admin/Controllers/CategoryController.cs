2b355720295fc304121c5144f8d0647dbc5d8b0e
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-12-04 18:34:09 +0300

    Kategoriler listelendi.

--- CoreProject/Areas/Admin/Controllers/CategoryController.cs
+++ CoreProject/Areas/Admin/Controllers/CategoryController.cs
@@ -3,15 +3,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
 
 namespace CoreProject.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CategoryController : Controller
     {
+        private CategoryManager _categoryManager = new CategoryManager(new EfCategoryRepository());
         public IActionResult Index()
         {
-            return View();
+            var values = _categoryManager.GetList();
+            return View(values);
         }
     }
 }
