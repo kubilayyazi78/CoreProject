f012526adc769066d37d97530d0582aaf92a47a5
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-12-11 23:48:35 +0300

    Excel export işlemlerine başlandı.

---
+++ CoreProject/Areas/Admin/Controllers/BlogController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ClosedXML.Excel;
+using CoreProject.Areas.Admin.Models;
+
+namespace CoreProject.Areas.Admin.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult ExportStaticExcelBlogList()
+        {
+            using (var workBook = new XLWorkbook())
+            {
+                var workSheet = workBook.Worksheets.Add("Blog Listesi");
+                workSheet.Cell(1, 1).Value = "Blog ID";
+                workSheet.Cell(1, 2).Value = "Blog Adı";
+
+                int blogRowCount = 2;
+
+                foreach (var item in GetBlogList())
+                {
+                    workSheet.Cell(blogRowCount, 1).Value = item.Id;
+                    workSheet.Cell(blogRowCount, 2).Value = item.Name;
+                    blogRowCount++;
+                }
+
+                using (var stream = new MemoryStream())
+                {
+                    workBook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application / vnd.openxmlformats - officedocument.spreadsheetml.sheet",
+                        "Calisma1.xlx");
+                }
+            }
+
+        }
+
+        public List<BlogModel> GetBlogList()
+        {
+            List<BlogModel> blogModels = new List<BlogModel>
+            {
+                new BlogModel { Id = 1, Name = "C#" },
+                new BlogModel { Id = 2, Name = "Java" },
+                new BlogModel { Id = 3, Name = "C++" }
+
+            };
+            return blogModels;
+        }
+    }
+}
