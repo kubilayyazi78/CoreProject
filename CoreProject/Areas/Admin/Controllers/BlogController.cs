f04cabf5897137766327e565a50ffad75c531812
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-12-12 18:10:58 +0300

    Blogların listesi excel olarak export edildi.

--- CoreProject/Areas/Admin/Controllers/BlogController.cs
+++ CoreProject/Areas/Admin/Controllers/BlogController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using ClosedXML.Excel;
 using CoreProject.Areas.Admin.Models;
+using DataAccessLayer.Concrete;
 
 namespace CoreProject.Areas.Admin.Controllers
 {
@@ -53,6 +54,53 @@ namespace CoreProject.Areas.Admin.Controllers
         }
 
         public IActionResult BlogListExcel()
+        {
+            return View();
+        }
+
+        public IActionResult ExportDynamicExcelBlogList()
+        {
+            using (var workBook = new XLWorkbook())
+            {
+                var workSheet = workBook.Worksheets.Add("Blog Listesi");
+                workSheet.Cell(1, 1).Value = "Blog ID";
+                workSheet.Cell(1, 2).Value = "Blog Adı";
+
+                int blogRowCount = 2;
+
+                foreach (var item in BlogTitleList())
+                {
+                    workSheet.Cell(blogRowCount, 1).Value = item.Id;
+                    workSheet.Cell(blogRowCount, 2).Value = item.Name;
+                    blogRowCount++;
+                }
+
+                using (var stream = new MemoryStream())
+                {
+                    workBook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                        "Calisma1.xlsx");
+                }
+            }
+        }
+
+        public List<BlogExcelModel> BlogTitleList()
+        {
+            List<BlogExcelModel> blogExcelModels = new List<BlogExcelModel>();
+            using (var context = new Context())
+            {
+                blogExcelModels = context.Blogs.Select(x => new BlogExcelModel
+                {
+                    Name = x.Title,
+                    Id = x.Id
+                }).ToList();
+            }
+
+            return blogExcelModels;
+        }
+
+        public IActionResult BlogTitleListExcel()
         {
             return View();
         }
