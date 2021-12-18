a5f7b724b812fc2e5d7f06fd59b4346507b413fa
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-12-19 00:54:03 +0300

    İstatistikler sayfası geliştirilmeye devam edildi.

---
+++ CoreProject/Areas/Admin/ViewComponents/Statistic/Statistic2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic2:ViewComponent
+    {
+        private Context _context = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.LastBlog = _context.Blogs.OrderByDescending(x => x.Id).Select(x => x.Title).Take(1)
+                .FirstOrDefault();
+            ViewBag.CommentCount = _context.Comments.Count();
+            return View();
+        }
+    }
+}
