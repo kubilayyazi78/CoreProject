b6d55f3b65eabdb7e2b40be3d355512b91180979
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-12-19 01:44:59 +0300

    Admin bilgileri getirildi.

---
+++ BusinessLayer/Concrete/AdminManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+    public class AdminManager : IAdminService
+    {
+        private IAdminDal _adminDal;
+
+        public AdminManager(IAdminDal adminDal)
+        {
+            _adminDal = adminDal;
+        }
+
+        public void Add(Admin entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Admin entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Admin GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Admin> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Admin entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- CoreProject/Areas/Admin/ViewComponents/Statistic/Statistic4.cs
+++ CoreProject/Areas/Admin/ViewComponents/Statistic/Statistic4.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataAccessLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreProject.Areas.Admin.ViewComponents.Statistic
 {
-    public class Statistic4:ViewComponent
+    public class Statistic4 : ViewComponent
     {
+        private Context _context = new Context();
         public IViewComponentResult Invoke()
         {
+            ViewBag.Name = _context.Admins.Where(x => x.Id == 1).Select(y => y.Name).FirstOrDefault();
+            ViewBag.Image = _context.Admins.Where(x => x.Id == 1).Select(y => y.ImageUrl).FirstOrDefault();
+            ViewBag.Desc = _context.Admins.Where(x => x.Id == 1).Select(y => y.ShortDescription).FirstOrDefault();
             return View();
         }
     }
