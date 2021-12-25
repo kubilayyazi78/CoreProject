c7fe4a7838844b05ea98e56ef55476a6a3da9893
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-12-25 23:58:29 +0300

    Api'den hava durumu çekildi.

--- CoreProject/Areas/Admin/ViewComponents/Statistic/Statistic.cs
+++ CoreProject/Areas/Admin/ViewComponents/Statistic/Statistic.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
@@ -18,6 +19,12 @@ namespace CoreProject.Areas.Admin.ViewComponents.Statistic
             ViewBag.BlogCount = _blogManager.GetList().Count();
             ViewBag.ContactCount = _context.Contacts.Count();
             ViewBag.CommentCount = _context.Comments.Count();
+
+            string api = "4c0d8e3843a5fe34e8a9f96bdac3b797";
+            string connection =
+                "http://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr%units=metric&appid=" + api;
+            XDocument document=XDocument.Load(connection);
+            ViewBag.temp = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
             return View();
         }
     }
