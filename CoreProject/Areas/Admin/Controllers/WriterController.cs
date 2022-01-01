5f7d471dcd8c2896433e2256b47b7a4c391fec75
kubilayyazi78 <kubilayyazi@hotmail.com>
2022-01-01 23:53:24 +0300

    Ajax ile static listeleme işlemi yapıldı.

---
+++ CoreProject/Areas/Admin/Controllers/WriterController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreProject.Areas.Admin.Models;
+using Newtonsoft.Json;
+
+namespace CoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class WriterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult WriterList()
+        {
+            var jsonWriters = JsonConvert.SerializeObject(Writers);
+            return Json(jsonWriters);
+        }
+
+        public List<WriterModel> Writers = new List<WriterModel>
+        {
+            new WriterModel
+            {
+                Id = 1,
+                Name = "Ahmet"
+            },
+            new WriterModel
+            {
+                Id = 2,
+                Name = "Veli"
+            },
+            new WriterModel
+            {
+                Id = 3,
+                Name = "Mehmet"
+            }
+        };
+    }
+}
