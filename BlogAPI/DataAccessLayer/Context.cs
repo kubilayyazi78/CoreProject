4e3956121612aaf1f8d30f9e9206d840519392db
kubilayyazi78 <kubilayyazi@hotmail.com>
2022-01-16 00:03:04 +0300

    API giriş yapıldı.

---
+++ BlogAPI/DataAccessLayer/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogAPI.DataAccessLayer
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CoreBlogApiDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
