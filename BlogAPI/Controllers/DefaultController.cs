a087fb1fe4431c715229040c881aedcf61a87844
kubilayyazi78 <kubilayyazi@hotmail.com>
2022-01-16 00:15:53 +0300

    Swagger ile datalar listelendi.

---
+++ BlogAPI/Controllers/DefaultController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogAPI.DataAccessLayer;
+
+namespace BlogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult List()
+        {
+            using var context = new Context();
+            var values = context.Employees.ToList();
+            return Ok();
+        }
+    }
+}
