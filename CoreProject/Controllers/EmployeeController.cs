0d31e411c743a5822c3d495bdf7eee448a82632b
kubilayyazi78 <kubilayyazi@hotmail.com>
2022-02-17 00:06:32 +0300

    Api kullanarak employee listeleme ve ekleme işlemi yapıldı.

---
+++ CoreProject/Controllers/EmployeeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CoreProject.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var httpClient = new HttpClient();
+            var responseMessage = await httpClient.GetAsync("https://localhost:44364/api/Default");
+            var jsonString = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<EmployeeClass>>(jsonString);
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee(EmployeeClass employeeClass)
+        {
+            var httpClient = new HttpClient();
+            var jsonEmployee = JsonConvert.SerializeObject(employeeClass);
+            StringContent content = new StringContent(jsonEmployee, Encoding.UTF8, "application/json");
+            var responseMessage = await httpClient.PostAsync("https://localhost:44364/api/Default", content);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(employeeClass);
+        }
+
+    }
+
+
+    public class EmployeeClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
