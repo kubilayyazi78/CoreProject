7999cbc525be7ba397e8877650a434fb97a7c6cc
kubilayyazi78 <kubilayyazi@hotmail.com>
2022-03-22 23:28:17 +0300

    Yazar işlemleri tamamlandı.

--- CoreProject/ViewComponents/Writer/MessageNotification.cs
+++ CoreProject/ViewComponents/Writer/MessageNotification.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,9 +12,13 @@ namespace CoreProject.ViewComponents.Writer
     public class MessageNotification : ViewComponent
     {
         private CommunicationManager _communicationManager = new CommunicationManager(new EfCommunicationRepository());
+        private Context _context = new Context();
         public IViewComponentResult Invoke()
         {
-            var values = _communicationManager.GetInboxListByWriter(1);
+            var userName = User.Identity.Name;
+            var userMail = _context.Users.Where(x => x.UserName == userName).Select(y => y.Email).FirstOrDefault();
+            var writerId = _context.Writers.Where(x => x.Mail == userMail).Select(y => y.Id).FirstOrDefault();
+            var values = _communicationManager.GetInboxListByWriter(writerId);
             return View(values);
         }
     }
