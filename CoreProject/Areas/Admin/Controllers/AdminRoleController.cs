38d58fdb0317e2d8b73a8e2e329b4472d1908a52
kubilayyazi78 <kubilayyazi@hotmail.com>
2022-04-03 17:23:46 +0300

    Role ekleme ve listeleme işlemi gerçekleştirildi.

---
+++ CoreProject/Areas/Admin/Controllers/AdminRoleController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreProject.Models;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+
+namespace CoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminRoleController : Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public AdminRoleController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _roleManager.Roles.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddRole()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddRole(RoleViewModel roleViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                AppRole appRole = new AppRole
+                {
+                    Name = roleViewModel.Name
+                };
+                var result = await _roleManager.CreateAsync(appRole);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View(roleViewModel);
+        }
+    }
+}
