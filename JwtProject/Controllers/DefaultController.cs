b152ad3ec5af39274abf9ba26dd980d071a28c4f
kubilayyazi78 <kubilayyazi@hotmail.com>
2022-04-09 15:59:28 +0300

    Token oluşturuldu ve postman üzerinden test edildi.

---
+++ JwtProject/Controllers/DefaultController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JwtProject.DAL;
+using Microsoft.AspNetCore.Authorization;
+
+namespace JwtProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult Login()
+        {
+            return Created("", new BuildToken().CreateToken());
+        }
+
+        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult Page()
+        {
+            return Ok("Başarılı");
+        }
+    }
+}
--- JwtProject/Startup.cs
+++ JwtProject/Startup.cs
@@ -60,7 +60,7 @@ namespace JwtProject
             }
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
